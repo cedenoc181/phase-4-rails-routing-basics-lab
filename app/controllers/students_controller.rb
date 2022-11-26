367843b9a7fc76bcbe6f26b5af0aa1dea7715fac
Christian <cedenoc181@gmail.com>
2022-11-25 22:04:02 -0500

    commit tree 88ff3787c95d32a540621310f16b05c215eee720

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index 
+        students = Student.all    
+        render json: students
+    end 
+
+    def grades
+    grades = Student.all.order(:grade).reverse
+    render json: grades
+    end
+
+    def highest_grade
+        highest_grade = Student.all.order(:grade).reverse.first
+        render json: highest_grade
+    end
+
+end
